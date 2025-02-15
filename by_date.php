68d64e2591449434d00ea0d48acb34571ef2d0b6
Anton Smirnov <sandfox@sandfox.me>
2025-02-15 18:27:37 +0200

    Check for all releases

---
+++ by_date.php
@@ -0,0 +1,17 @@
+<?php
+
+$packages = json_decode(file_get_contents(__DIR__ . '/composer.lock'), true)['packages'];
+
+foreach ($packages as &$p) {
+    $p['time_obj'] = new DateTimeImmutable($p['time']);
+}
+unset($p);
+
+usort($packages, fn ($a, $b) => $a['time_obj'] <=> $b['time_obj']);
+
+foreach ($packages as $p) {
+    if (!str_starts_with($p['name'], 'arokettu/')) {
+        continue;
+    }
+    echo sprintf("%s    %-34s %s\n", $p['time_obj']->format('Y-m-d'), $p['name'], $p['version']);
+}
